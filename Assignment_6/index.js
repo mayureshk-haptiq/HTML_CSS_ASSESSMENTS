9d8d1d88164b5f1b537d21ce343d9c24b5ddc30b
Mayuresh_Kumkar <mayuresh.kumkar@haptiq.com>
2024-12-09 12:54:03 +0530

    Assignment 6

---
+++ Assignment_6/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".categories").slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      infinite : true,
+      prevArrow: $('.customPrev'),
+      nextArrow:$('.customNext'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ],
+    });
+  });
